d8d8a221bcb508b4cc41eed118fa78969baef3e2
danikenan <danikenan@gmail.com>
2016-02-29 22:39:51 +0200

    Update index.js

--- auth_server/src/index.js
+++ auth_server/src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const koa = require('koa');
 const static = require('koa-static');
 const logger = require('koa-logger');
-const PORT = process.env.PORT || 8000;
+const port = require('./config').port;
 const routes = require('./routes');
 
 const app = koa();
@@ -10,6 +10,6 @@ const app = koa();
 app.use(logger());
 app.use(routes());
 app.use(static(path.join(__dirname, '../public')));
-app.listen(PORT);
+app.listen(port);
 
-console.log(`Server is Running on port ${PORT}`);
+console.log(`Server is Running on port ${port}`);
