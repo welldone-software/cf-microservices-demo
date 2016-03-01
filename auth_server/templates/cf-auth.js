499923f174128017ea439ff99b52674213df712b
yairov <yairov@gmail.com>
2016-03-01 18:35:09 +0200

    improve login dialog

--- auth_server/templates/cf-auth.js
+++ auth_server/templates/cf-auth.js
@@ -1,9 +1,20 @@
 function cfAuthOpen(appId, cb){
   var authServerBaseUrl = '__AUTH_BASE_URL__';
-  var win = window.open(authServerBaseUrl + '/' + appId + '/login', 'auth' , 'width=500, height=300, menubar=no, topbar=no, status=no, resizable=no, location=no');
+  var width = 300;
+  var height = 379;
+  var top = (screen.height - height) / 2;
+  var left = (screen.width - width) / 2;
+  var features = 'menubar=0,location=1,resizable=1,scrollbars=0,status=0,toolbar=0,width='+width+',height='+height+',left='+left+',top='+top;
+  var url = authServerBaseUrl + '/' + appId + '/login';
+  var win = window.open(url, 'oauth', features);
 
   window.addEventListener('message', function(event){
-    var eventData = JSON.parse(event.data);
+    var eventData = typeof event.data === 'string'
+      ? JSON.parse(event.data)
+      : event.data;
+
+    if (eventData.source) return;
+
     if (eventData.success) {
       cb(null, eventData.jwt);
     } else {
